b0334c43c15d2096d1d4182c380ecddbe289eb5c
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-27 05:01:46 -0500

    Adjusting prompts

--- src/components/hooks/usePrompt.jsx
+++ src/components/hooks/usePrompt.jsx
@@ -1,11 +1,9 @@
 const prompts = [
 	'What needs to get done?',
 	'One thing to make tomorrow better.',
-	'Write it down so you dont forget.',
-	"It's right on the tip of my tongue.",
-	'One less thing to remember.',
-	'Your reminder to call them.',
+	"Write it down so you don't forget.",
 	"What's on the agenda?",
+	'Make today a win.',
 ]
 
 export function usePrompt() {
